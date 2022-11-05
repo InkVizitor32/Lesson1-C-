70c286baa83a92353109a2f0e1ebabde3f682d67
InkVizitor32 <morgunov.roman89@gmail.com>
2022-11-05 14:34:15 +0300

    docs: протестил программу

--- Program.cs
+++ Program.cs
@@ -52,20 +52,20 @@
 //     // Напишите программу, которая будет выдавать название дня недели по заданному номеру.
 //     // 3 -> Среда
 //     // 5 -> Пятница 
-// try
-//     {
-//     Console.WriteLine("Введите натуральное число от 1 до 7 ");
-//     int x=Convert.ToInt32( Console.ReadLine());
-//     if (x==1) Console.WriteLine("Понедельник");
-//     else if (x == 2) Console.WriteLine("Вторник");
-//     else if (x == 3) Console.WriteLine("Среда");
-//     else if (x == 4) Console.WriteLine("четверг");
-//     else if (x == 5) Console.WriteLine("пятница");
-//     else if (x == 6) Console.WriteLine("суббота");
-//     else if (x == 7) Console.WriteLine("воскресенье");  
-//     else Console.WriteLine("столько не бывает дней в неделе");
-//     }
-// catch
-// {
-//     Console.WriteLine("вы ввели чушь"); 
-// }
+try
+    {
+    Console.WriteLine("Введите натуральное число от 1 до 7 ");
+    int x=Convert.ToInt32( Console.ReadLine());
+    if (x==1) Console.WriteLine("Понедельник");
+    else if (x == 2) Console.WriteLine("Вторник");
+    else if (x == 3) Console.WriteLine("Среда");
+    else if (x == 4) Console.WriteLine("четверг");
+    else if (x == 5) Console.WriteLine("пятница");
+    else if (x == 6) Console.WriteLine("суббота");
+    else if (x == 7) Console.WriteLine("воскресенье");  
+    else Console.WriteLine("столько не бывает дней в неделе");
+    }
+catch
+{
+    Console.WriteLine("вы ввели чушь"); 
+}
